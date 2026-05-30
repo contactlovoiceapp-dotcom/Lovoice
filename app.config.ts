023423dd49d6423242c6c0371fbc7ad93c8b17df
freekerz <freekerz@users.noreply.github.com>
2026-05-30 11:58:58 +0200

    chore(release): bump version to 0.9.3

--- app.config.ts
+++ app.config.ts
@@ -10,7 +10,7 @@ const config: ExpoConfig = {
   name: "Lovoice",
   slug: "lovoice",
   scheme: "lovoice",
-  version: "0.9.2",
+  version: "0.9.3",
   orientation: "portrait",
   icon: "./assets/icon.png",
   userInterfaceStyle: "light",
