db10f8e88fab5a863bcb49e099782cda28c8bbcf
freekerz <freekerz@users.noreply.github.com>
2026-05-31 18:18:43 +0200

    v0.9.6

--- app.config.ts
+++ app.config.ts
@@ -10,7 +10,7 @@ const config: ExpoConfig = {
   name: "Lovoice",
   slug: "lovoice",
   scheme: "lovoice",
-  version: "0.9.5",
+  version: "0.9.6",
   orientation: "portrait",
   icon: "./assets/icon.png",
   userInterfaceStyle: "light",
